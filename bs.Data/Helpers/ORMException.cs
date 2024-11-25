eb38dd5307b11ec16e8470bfac56fb9b5259beab
Fabio Cavallari @ITALCOM <fcavallari@italcom.it>
2024-11-25 09:58:12 +0100

    migrato a net 8

--- bs.Data/Helpers/ORMException.cs
+++ bs.Data/Helpers/ORMException.cs
@@ -44,15 +44,6 @@ namespace bs.Data.Helpers
             ExceptionOrigin = exceptionOrigin;
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ORMException"/> class.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination.</param>
-        protected ORMException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
-
         public string ExceptionOrigin { get; }
     }
 }
--- bs.Data/Helpers/ORMValidationException.cs
+++ bs.Data/Helpers/ORMValidationException.cs
@@ -35,15 +35,6 @@ namespace bs.Data.Helpers
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ORMValidationException"/> class.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination.</param>
-        protected ORMValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
-
-        /// <summary>
         /// Gets or sets the validation errors.
         /// </summary>
         /// <value>
